f66ceab74d7d7c370f4d009db975a1a9c1e2990c
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-10-28 10:31:27 +0530

    done

--- Program-3.c
+++ Program-3.c
@@ -8,13 +8,15 @@
 //Note: Do use the printf() function with a newline character (\n).
 
 #include <stdio.h>
-void main() {
-	int i = 0, n;
-	printf("Enter an integer : ");
+int main() {
+	int i = 1, n;
+	printf("Enter an integer");
+	scanf("%d",&n);
  // Fill the code in scanf()
 	do {
-		printf("I Love VVCE\n");
+		printf("I Love VVCE");
+		i++:
 		// complete the statement
-	}  // complete the condition
+	} while(i<=n);
+	return(0);// complete the condition
 }
-
