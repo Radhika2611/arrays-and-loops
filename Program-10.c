3efcdad1c4531aadecf78d45e85386880af36055
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-12-16 16:28:06 +0530

    U

--- Program-10.c
+++ Program-10.c
@@ -1,22 +1,5 @@
-/*Write a program to read an array of integers (with max size 10) and print the largest and the smallest of the given numbers.
-
-At the time of execution, the program should print the message on the console as:
-Enter how many values you want to read : 
-For example, if the user gives the input as:
-Enter how many values you want to read : 4
-Next, the program should print the messages one by one on the console as:
-Enter the value of a[0] : 
-Enter the value of a[1] :
-Enter the value of a[2] :
-Enter the value of a[3] : 
-If the user gives the input as:
-Enter the value of a[0] : 34
-Enter the value of a[1] : 23
-Enter the value of a[2] : 6
-Enter the value of a[3] : 7
-then the program should print the result as:
-The largest element of the array = 34
-The smallest element of the array = 6*/
+//INPUT - 4 30 20 10 60
+//OUTPUT: The largest and smallest elements are 60 and 10
 
 //INSERT THE MISSING CODE 
 #include <stdio.h>
