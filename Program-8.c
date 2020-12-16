a38f8d33f16ffc09ca1bad1f3c18eb8be1235af5
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-12-16 16:43:14 +0530

    U

--- Program-8.c
+++ Program-8.c
@@ -1,21 +1,5 @@
-
-/*Write a program that iterates over the array and counts the number of times the element occurs in the array (with max size 10), and finally prints the value of the count.
-At the time of execution, the program should print the message on the console as:
-Enter size of the array : 
-For example, if the user gives the input as:
-Enter size of the array : 5
-Next, the program should print the message on the console as:
-Enter array elements : 
-If the user gives the input as:
-Enter array elements : 10 20 20 30 10
-Next, the program should print the message on the console as:
-Enter an integer value : 
-If the user gives the input as:
-Enter an integer value : 10
-then the program should print the result as:
-Number of times element 10 is repeated : 2*/
-
-//INSERT THE MISSING CODE 
+//INPUT- 4 10 20 20 30 20
+//OUTPUT - The number of times the  key element  is repeated  is 2
 #include<stdio.h>
 int main(){
   int n,flag=0,i,key,a[10],c=0;
