aa91948660aa8340fe931a2b732a241619ce36de
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-12-16 16:29:25 +0530

    U

--- Program-2.c
+++ Program-2.c
@@ -1,13 +1,5 @@
-//Write a C program to print first n natural numbers using while loop
-//At the time of execution, the program should print the message on the console as:
-//Enter upper limit : 
-//For example, if the user gives the input as :
-//Enter upper limit : 3
-//then the program should print the result as:
-//The natural numbers from 1 - 3 : 1 2 3
-//Note: Do use the printf() function with a newline character (\n) at the end.
-
-
+//INPUT -3
+//OUTPUT - 1 2 3
 
 
 #include<stdio.h>
