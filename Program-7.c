a6328833bcbf46ca97c5cb1855e7afc86a557d40
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-12-16 16:37:03 +0530

    U

--- Program-7.c
+++ Program-7.c
@@ -1,14 +1,5 @@
-/*Write a program to print the given integer elements of an array (with max size 10) in reverse order.
-At the time of execution, the program should print the message on the console as:
-Enter size of the array : 
-For example, if the user gives the input as:
-Enter size of the array : 3
-Next, the program should print the message on the console as:
-Enter array elements : 
-If the user gives the input as:
-Enter array elements : 10 20 30
-then the program should print the result as:
-Array elements in reverse order : 30 20 10*/
+//INPUT - 3 1 2 3
+//OUTPUT- 3 2 1
 
 //INSERT THE MISSING CODE 
 #include<stdio.h>
