6a144bf7414040f3ac625df8789e1e2df3280056
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-12-16 16:39:20 +0530

    U

--- Program-6.c
+++ Program-6.c
@@ -1,16 +1,5 @@
-
-/*Write a program to read an array of integers (with max size 10) and print the total number of even numbers and odd numbers in that array.
-At the time of execution, the program should print the message on the console as:
-Enter array size: 
-For example, if the user gives the input as:
-Enter array size: 4
-Next, the program should print the message on the console as:
-Enter 4 elements: 
-If the user gives the input as:
-Enter 4 elements : 23 45 22 61
-then the program should print the result as:
-Total number of even numbers in the array : 1
-Total number of odd numbers in the array : 3*/
+//INPUT -3 1 2 3
+//OUTPUT- Total number of even numbers and odd numbers in the array are 1 and 2
 
 //INSERT THE MISSING CODE 
 
