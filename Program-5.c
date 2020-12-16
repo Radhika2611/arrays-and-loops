6854b5b105ba643545bd1f95a86d2373348abb80
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-12-16 16:34:29 +0530

    U

--- Program-5.c
+++ Program-5.c
@@ -1,22 +1,6 @@
-/* Write a program to insert an element at a specified position in an array
-Enter number of elements in array
-3
-Enter 3 elements
-45
-78
-98
-Enter the location where you wish to insert an element
-2
-Enter the value to insert
-60
-Resultant array is
-45
-60
-78
-98
-*/
+//INPUT - 4 1 2 3 4
+//OUTPUT - Sum of elements=10
 
-//INSERT THE MISSING CODE 
 
 #include <stdio.h>
  
